172ef628b01a5efc72b7b7429f514728c15c2079
João Pedro Gonçalves Viana de Souza <107888675+JoaoVianaSouza@users.noreply.github.com>
2024-06-15 22:47:08 -0300

    Delete DIA 16-20/Projeto - ScreenMatch directory

--- DIA 16-20/Projeto - ScreenMatch/src/screenmatch/Main/TituloOmdb.java
+++
@@ -1,8 +0,0 @@
-package screenmatch.Main;
-
-// Record é um cabeçalho sem muitas informações
-// Definindo o que será passado nessa classe, que serão as informações do json:
-// Ele irá pegar e escrever o campo "title", "year" e "runtime"
-public record TituloOmdb(String title, String year, String runtime) {
-    
-}
